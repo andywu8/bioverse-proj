49525bcf7ffe228bc7e1184b954b0322f376f2b1
Andy Wu <andyww09@gmail.com>
2023-11-05 21:09:06 -0800

    Add submit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Panel from './components/Panel';
 import TicketSupport from './components/TicketSupport';
 function App() {
   return (
-  <Router>
-    <ul>
-      <li>
-        <Link to="/ticket_support">Ticket Support</Link>
-      </li>
-      <li>
-        <Link to="/panel">Panel</Link>
-      </li>
-    </ul>
-    <Routes>
-      <Route exact path='/panel' element={< Panel />}></Route>  
-      <Route exact path='/ticket_support' element={< TicketSupport />}></Route>  
-    </Routes>
-  </Router>
+    <div className="App">  
+    <Router>
+      <ul>
+        <li>
+          <Link to="/ticket_support">Ticket Support</Link>
+        </li>
+        <li>
+          <Link to="/panel">Panel</Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route exact path='/panel' element={< Panel />}></Route>  
+        <Route exact path='/ticket_support' element={< TicketSupport />}></Route>  
+      </Routes>
+    </Router>
+    </div>
   );
 }
 
--- frontend/src/components/Panel.js
+++ frontend/src/components/Panel.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
 
-export default function Panel(){
-    return(
-        <div>
-            <h1>Panel</h1>
+export default function Panel() {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Panel</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="form-group">
+          <label for="email">Email address</label>
+          <input
+            {...register("email")}
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+          ></input>
+          <label for="name">Name</label>
+          <input
+            {...register("name")}
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter name"
+          ></input>
+          <label for="name">Description of Problem</label>
+          <input
+            {...register("description")}
+            type="text"
+            className="form-control"
+            id="name"
+            placeholder="Enter problem"
+          ></input>
         </div>
-    );
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
-
